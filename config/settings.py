44a67a5ba863eb50a2fe073f55641661a9b75393
ishabatool247 <ishabatool247@gmail.com>
2026-09-02 15:20:39 +0500

    Allow frontend Render origin

--- config/settings.py
+++ config/settings.py
@@ -197,6 +197,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://real-estate-analyzer-fronten.onrender.com",
 ]
 
 
@@ -208,9 +209,8 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://real-estate-analyzer-fronten.onrender.com",
 ]
-
-
 
 
 
